0be47411eeafc3ac92a00870ed53d7ebb42b2fdf
Delphik <max.henny@gmail.com>
2026-04-19 01:53:14 -0400

    Update sengled_motion_flood.py

--- sengled_motion_flood.py
+++ sengled_motion_flood.py
@@ -1,6 +1,7 @@
 from zigpy.quirks import CustomDevice
 from zigpy.quirks.v2 import CustomCluster
 import zigpy.types as t
+from zigpy.zcl import foundation
 from zigpy.zcl.clusters import general
 from zigpy.profiles import zha
 
@@ -8,9 +9,10 @@ class ManuSpecificSengledMotionSensor(CustomCluster):
     cluster_id = 0xfc01
     manufacturer_id = 0x1160
     name = "ManuSpecificSengledMotionSensor"
+
     attributes = {
-        0x0000: ("trigger_condition", t.uint8_t),
-        0x0001: ("enable_auto_on_off", t.Bool),
+        0x0000: foundation.ZCLAttributeDef("trigger_condition", type=t.uint8_t, is_manufacturer_specific=True),
+        0x0001: foundation.ZCLAttributeDef("enable_auto_on_off", type=t.Bool, is_manufacturer_specific=True),
     }
 
 class SengledE13N11(CustomDevice):
